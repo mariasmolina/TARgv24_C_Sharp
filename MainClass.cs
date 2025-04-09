dd43a537ce39272aee3c987ef976f34168767e0b
opilane <opilane@HP-CZC2349HTN>
2025-04-09 14:15:37 +0300

    gg

--- MainClass.cs
+++ MainClass.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TARgv24_C_
+namespace TARgv24_C
 {
     internal class MainClass
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // I. osa Andmetüübid, If, Case, Random, Alamfunktsioonid
+        }
     }
 }
