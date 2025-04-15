89d6c277ced7f62486eea755d792b58a5ad256cb
mariasmolina <mariia.smolina@gmail.com>
2025-04-15 14:00:35 +0300

    Tehtud klass Point

---
+++ Madu/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C_Sharp.Madu
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
