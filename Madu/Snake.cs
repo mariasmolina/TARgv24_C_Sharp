01887e42f7377e6d3d571b97cec1aaa4030adb47
mariasmolina <mariia.smolina@gmail.com>
2025-04-16 14:02:01 +0300

    Loeng 9 - klass madu tehtud

---
+++ Madu/Snake.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C_Sharp.Madu
+{
+    class Snake : Figure
+    {
+        public Snake(Point tail, int lenght, Direction direction)
+        {
+            pList = new List<Point>();
+            for (int i = 0; i < lenght; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+    }
+}
