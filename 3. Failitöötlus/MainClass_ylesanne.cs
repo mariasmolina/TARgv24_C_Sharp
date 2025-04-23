70c4f441d4adcb04377ceb7dacd051e05e4c82e6
opilane <opilane@HP-CZC2349HTN>
2025-04-23 16:07:57 +0300

    loodud uus klass MainClass_ylesanne - ülesandede jaoks

---
+++ 3. Failitöötlus/MainClass_ylesanne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C_Sharp._3._Failitöötlus
+{
+    internal class MainClass_ylesanne
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
