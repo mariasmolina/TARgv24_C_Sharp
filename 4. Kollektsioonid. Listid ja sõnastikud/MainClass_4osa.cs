c80bc93d38e7845516fc0d71b0cfbd2524204fd0
opilane <opilane@HP-CZC2349HTN>
2025-04-23 15:55:40 +0300

    loodud MainClass_4osa ja Person klassid

--- 4. Kollektsioonid. Listid ja sõnastikud/MainClass_4osa.cs
+++ 4. Kollektsioonid. Listid ja sõnastikud/MainClass_4osa.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,70 @@ namespace TARgv24_C_Sharp._4._Kollektsioonid._Listid_ja_sõnastikud
 {
     internal class MainClass_4osa
     {
+        public static void Main(string[] args)
+        {
+            // ----- 1. ArrayList (System.Collections) ------
+            ArrayList nimed = new ArrayList();
+            nimed.Add("Kati");
+            nimed.Add("Mati");
+            nimed.Add("Juku");
+
+            if (nimed.Contains("Mati"))
+                Console.WriteLine("Mati olemas");
+
+            Console.WriteLine("Nimesid kokku: " + nimed.Count);
+
+            nimed.Insert(1, "Sass");
+
+            Console.WriteLine("Mati indeks: " + nimed.IndexOf("Mati"));
+            Console.WriteLine("Mari indeks: " + nimed.IndexOf("Mari"));
+
+            foreach (string nimi in nimed)
+                Console.WriteLine(nimi);
+
+
+            // ------ 2. Tuple (järjendid) -------
+            Tuple<float, char> route = new Tuple<float, char>(2.5f, 'N');
+            Console.WriteLine($"Vahemaa: {route.Item1}, Suund: {route.Item2}");
+
+
+            // ------ 3. List (System.Collections.Generic) -------
+            List<Person> people = new List<Person>();
+            people.Add(new Person() { Name = "Kadi" });   // class Person.cs
+            people.Add(new Person() { Name = "Mirje" });
+            people.Add(new Person() { Name = "Juku" });
+
+            foreach (Person p in people)
+                Console.WriteLine(p.Name);
+
+
+            // --------- 4. LinkedList (System.Collections.Generic) --------
+            LinkedList<int> loetelu = new LinkedList<int>();
+            loetelu.AddLast(5);
+            loetelu.AddLast(3);
+            loetelu.AddFirst(0);
+
+            foreach (int arv in loetelu)
+                Console.WriteLine(arv);
+
+            loetelu.RemoveFirst();
+            loetelu.RemoveLast();
+            loetelu.AddLast(555);
+            loetelu.Remove(555);
+
+
+            // --------- 5. Dictionary<TKey, TValue> – Sõnastik --------
+            Dictionary<int, string> riigid = new Dictionary<int, string>();
+            riigid.Add(1, "Hiina");
+            riigid.Add(2, "Eesti");
+            riigid.Add(3, "Itaalia");
+
+            foreach (var paar in riigid)
+                Console.WriteLine($"{paar.Key} - {paar.Value}");
+
+            string pealinn = riigid[2];
+            riigid[2] = "Eestimaa";
+            riigid.Remove(3);
+        }
     }
 }
---
+++ 4. Kollektsioonid. Listid ja sõnastikud/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C_Sharp._4._Kollektsioonid._Listid_ja_sõnastikud
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+    }
+}
