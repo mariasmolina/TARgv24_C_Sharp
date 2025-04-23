88af2ef410ab3f350b7df0e4d3f0580d7385878c
opilane <opilane@HP-CZC2349HTN>
2025-04-23 17:45:29 +0300

    protsessis

--- 4. Kollektsioonid. Listid ja sõnastikud/MainClass_ylesanne.cs
+++ 4. Kollektsioonid. Listid ja sõnastikud/MainClass_ylesanne.cs
@@ -12,9 +12,20 @@ namespace TARgv24_C_Sharp._4._Kollektsioonid._Listid_ja_sõnastikud
         {
             // Ülesanne 1 – Kalorite kalkulaator klassidega
 
+            List<Toode> tooded = new List<Toode>();
 
-
-
+            tooded.Add(new Toode() { Nimi = "Õun", Kalorid100g = 52 });
+            tooded.Add(new Toode() { Nimi = "Banaan", Kalorid100g = 89 });
+            tooded.Add(new Toode() { Nimi = "Kartul", Kalorid100g = 77 });
+            tooded.Add(new Toode() { Nimi = "Tomat", Kalorid100g = 18 });
+            tooded.Add(new Toode() { Nimi = "Kurg", Kalorid100g = 16 });
+            tooded.Add(new Toode() { Nimi = "Porgand", Kalorid100g = 41 });
+            tooded.Add(new Toode() { Nimi = "Lõhe filee", Kalorid100g = 208 });
+            tooded.Add(new Toode() { Nimi = "Kana muna", Kalorid100g = 155 });
+            tooded.Add(new Toode() { Nimi = "Kanafilee", Kalorid100g = 165 });
+            tooded.Add(new Toode() { Nimi = "Sealiha", Kalorid100g = 242 });
+            tooded.Add(new Toode() { Nimi = "Veiseliha", Kalorid100g = 250 });
+            tooded.Add(new Toode() { Nimi = "Keedetud riis", Kalorid100g = 130 });
 
 
 
