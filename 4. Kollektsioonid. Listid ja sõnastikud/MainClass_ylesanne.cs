ce10837bcd5e87cd19e1f48cf0a5bb40728247ad
opilane <opilane@HP-CZC2349HTN>
2025-04-23 16:59:19 +0300

    loodud klass - MainClass_ylesanne

---
+++ 4. Kollektsioonid. Listid ja sõnastikud/MainClass_ylesanne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C_Sharp._4._Kollektsioonid._Listid_ja_sõnastikud
+{
+    internal class MainClass_ylesanne
+    {
+        public static void Main(string[] args)
+        {
+            // Ülesanne 1 – Kalorite kalkulaator klassidega
+
+
+
+
+
+
+
+        }
+    }
+}
