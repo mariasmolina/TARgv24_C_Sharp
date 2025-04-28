7498c3e5bb3323a410741c125cff04aa08786466
mariasmolina <mariia.smolina@gmail.com>
2025-04-28 14:47:36 +0300

    Ülesanne 2 (Maakonnad ja pealinnad) - tehtud

--- 5. osa - Kollektsioonid. Listid ja sõnastikud/MainClass_ylesanne.cs
+++ 5. osa - Kollektsioonid. Listid ja sõnastikud/MainClass_ylesanne.cs
@@ -10,102 +10,114 @@ namespace TARgv24_C_Sharp._5._osa___Kollektsioonid._Listid_ja_sõnastikud
     {
         public static void Main(string[] args)
         {
-
+            /********************************************************************************/
             // Ülesanne 1 – Kalorite kalkulaator klassidega
 
-            Console.WriteLine("--- Ülesanne 1 ---");
-            List<Toode> tooted = new List<Toode>();
+            //Console.WriteLine("--- Ülesanne 1 ---");
+            //List<Toode> tooted = new List<Toode>();
 
-            tooted.Add(new Toode() { Nimi = "Õun", Kalorid100g = 52 });
-            tooted.Add(new Toode() { Nimi = "Banaan", Kalorid100g = 89 });
-            tooted.Add(new Toode() { Nimi = "Kartul", Kalorid100g = 77 });
-            tooted.Add(new Toode() { Nimi = "Tomat", Kalorid100g = 18 });
-            tooted.Add(new Toode() { Nimi = "Kurg", Kalorid100g = 16 });
-            tooted.Add(new Toode() { Nimi = "Porgand", Kalorid100g = 41 });
-            tooted.Add(new Toode() { Nimi = "Lõhe filee", Kalorid100g = 208 });
-            tooted.Add(new Toode() { Nimi = "Kana muna", Kalorid100g = 155 });
-            tooted.Add(new Toode() { Nimi = "Kanafilee", Kalorid100g = 165 });
-            tooted.Add(new Toode() { Nimi = "Sealiha", Kalorid100g = 242 });
-            tooted.Add(new Toode() { Nimi = "Veiseliha", Kalorid100g = 250 });
-            tooted.Add(new Toode() { Nimi = "Keedetud riis", Kalorid100g = 130 });
-
-
-            // Kasutaja andmed
-            Console.Write("Sisestage oma nimi: ");
-            string nimi = Console.ReadLine();
-            Console.Write("Sisestage oma vanus: ");
-            int vanus = int.Parse(Console.ReadLine());
-            Console.Write("Sisestage sugu (mees/naine): ");
-            string sugu = Console.ReadLine();
-            Console.Write("Sisestage pikkus (cm): ");
-            double pikkus = double.Parse(Console.ReadLine());
-            Console.Write("Sisestage kaal (kg): ");
-            double kaal = double.Parse(Console.ReadLine());
-            Console.Write("-- Aktiivsus tasemed --\n1. Istuv eluviis\n2. Kerge aktiivsus\n3. Mõõdukas aktiivsus\n4. Väga aktiivne\n5. Äärmiselt aktiivne\nValige aktiivsustase (1-5): ");
-            double aktiivsus = double.Parse(Console.ReadLine());
-
-            Inimene inimene = new Inimene()
-            {
-                Nimi = nimi,
-                Vanus = vanus,
-                Sugu = sugu,
-                Pikkus = pikkus,
-                Kaal = kaal,
-                Aktiivsustase = aktiivsus
-            };
-
-            // Päevase energiavajaduse arvutamine Harris-Benedict’i valemi abi
-            // https://nutrium.com/blog/harris-benedict-equation-calculator-for-nutrition-professionals/
-
-            double paevaneEnergiavajadus = Math.Round(inimene.ArvutaKalorivajadus(), 2);
-
-            Console.WriteLine($"\n{nimi}, sinu hinnanguline päevane kalorivajadus on {paevaneEnergiavajadus} kcal\n");
-
-            foreach (Toode t in tooted)
-            {
-                double kogus = Math.Round(paevaneEnergiavajadus / t.Kalorid100g * 100,2);
-                Console.WriteLine($"{t.Nimi}: {kogus} g päevas");
-            }
+            //tooted.Add(new Toode() { Nimi = "Õun", Kalorid100g = 52 });
+            //tooted.Add(new Toode() { Nimi = "Banaan", Kalorid100g = 89 });
+            //tooted.Add(new Toode() { Nimi = "Kartul", Kalorid100g = 77 });
+            //tooted.Add(new Toode() { Nimi = "Tomat", Kalorid100g = 18 });
+            //tooted.Add(new Toode() { Nimi = "Kurg", Kalorid100g = 16 });
+            //tooted.Add(new Toode() { Nimi = "Porgand", Kalorid100g = 41 });
+            //tooted.Add(new Toode() { Nimi = "Lõhe filee", Kalorid100g = 208 });
+            //tooted.Add(new Toode() { Nimi = "Kana muna", Kalorid100g = 155 });
+            //tooted.Add(new Toode() { Nimi = "Kanafilee", Kalorid100g = 165 });
+            //tooted.Add(new Toode() { Nimi = "Sealiha", Kalorid100g = 242 });
+            //tooted.Add(new Toode() { Nimi = "Veiseliha", Kalorid100g = 250 });
+            //tooted.Add(new Toode() { Nimi = "Keedetud riis", Kalorid100g = 130 });
 
 
+            //// Kasutaja andmed
+            //Console.Write("Sisestage oma nimi: ");
+            //string nimi = Console.ReadLine();
+            //Console.Write("Sisestage oma vanus: ");
+            //int vanus = int.Parse(Console.ReadLine());
+            //Console.Write("Sisestage sugu (mees/naine): ");
+            //string sugu = Console.ReadLine();
+            //Console.Write("Sisestage pikkus (cm): ");
+            //double pikkus = double.Parse(Console.ReadLine());
+            //Console.Write("Sisestage kaal (kg): ");
+            //double kaal = double.Parse(Console.ReadLine());
+            //Console.Write("-- Aktiivsus tasemed --\n1. Istuv eluviis\n2. Kerge aktiivsus\n3. Mõõdukas aktiivsus\n4. Väga aktiivne\n5. Äärmiselt aktiivne\nValige aktiivsustase (1-5): ");
+            //double aktiivsus = double.Parse(Console.ReadLine());
+
+            //Inimene inimene = new Inimene()
+            //{
+            //    Nimi = nimi,
+            //    Vanus = vanus,
+            //    Sugu = sugu,
+            //    Pikkus = pikkus,
+            //    Kaal = kaal,
+            //    Aktiivsustase = aktiivsus
+            //};
+
+            //// Päevase energiavajaduse arvutamine Harris-Benedict’i valemi abi
+            //// https://nutrium.com/blog/harris-benedict-equation-calculator-for-nutrition-professionals/
+
+            //double paevaneEnergiavajadus = Math.Round(inimene.ArvutaKalorivajadus(), 2);
+
+            //Console.WriteLine($"\n{nimi}, sinu hinnanguline päevane kalorivajadus on {paevaneEnergiavajadus} kcal\n");
+
+            //foreach (Toode t in tooted)
+            //{
+            //    double kogus = Math.Round(paevaneEnergiavajadus / t.Kalorid100g * 100,2);
+            //    Console.WriteLine($"{t.Nimi}: {kogus} g päevas");
+            //}
+
+
+
+            /********************************************************************************/
             // Ülesanne 2 – Maakonnad ja pealinnad (sõnastik ja test)
 
             Console.WriteLine("\n\n--- Ülesanne 2 ---");
 
-            bool jatka = true;
-            while (jatka)
+            while (true)
             {
-                Console.WriteLine("\nVali tegevus:");
+                Console.WriteLine("\n--- Maakonnad ja pealinnad ---");
                 Console.WriteLine("1. Otsi maakonda või linna");
                 Console.WriteLine("2. Lisa uus maakond");
                 Console.WriteLine("3. Mängurežiim");
                 Console.WriteLine("4. Kuvada kõik maakonnad ja pealinnad");
                 Console.WriteLine("5. Välju");
 
+                Console.Write("\nVali tegevus: ");
                 string valik = Console.ReadLine();
-                switch (valik)
+
+                if (valik == "1")
                 {
-                    case "1":
-                        FunktsioonideClass_5osa.OtsiMaakondVoiLinn();
-                        break;
-                    case "2":
-                        FunktsioonideClass_5osa.LisaUusMaakond();
-                        break;
-                    case "3":
-                        FunktsioonideClass_5osa.ManguReziim();
-                        break;
-                    case "4":
-                        FunktsioonideClass_5osa.KuvaKoik();
-                        break;
-                    case "5":
-                        jatka = false;
-                        Console.WriteLine("Head aega!");
-                        break;
-                    default:
-                        Console.WriteLine("Vale valik. Proovi uuesti.");
-                        break;
+                    FunktsioonideClass_5osa.OtsiMaakondLinn();
+                }
+                else if (valik == "2")
+                {
+                    FunktsioonideClass_5osa.LisaUusMaakond();
+                }
+                else if (valik == "3")
+                {
+                    FunktsioonideClass_5osa.ManguReziim();
+                }
+                else if (valik == "4")
+                {
+                    FunktsioonideClass_5osa.KuvaKoik();
+                }
+                else if (valik == "5")
+                {
+                    Console.WriteLine("Head aega!");
+                    break;  // выходим из цикла
+                }
+                else
+                {
+                    Console.WriteLine("Vale valik. Proovi uuesti.");
                 }
             }
+
+
+
+            /********************************************************************************/
+            // Ülesanne 3 – Õpilased ja hinnete analüüs
+
         }
     }
 }
