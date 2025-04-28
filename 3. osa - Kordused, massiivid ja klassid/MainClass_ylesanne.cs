abea6b7607096c8b071a01d638fc9d0a65a70e68
mariasmolina <mariia.smolina@gmail.com>
2025-04-28 19:14:55 +0300

    Ülesanne 5 (Arvamise mäng) - tehtud

--- 3. osa - Kordused, massiivid ja klassid/MainClass_ylesanne.cs
+++ 3. osa - Kordused, massiivid ja klassid/MainClass_ylesanne.cs
@@ -26,36 +26,70 @@ namespace TARgv24_C_Sharp._3._osa___Kordused__massiivid_ja_klassid
 
             /************************************************************/
             // Ülesanne 4 - "Osta elevant ära!"
-            static void KuniMärksõnani(string märksõna, string fraas)
-            {
-                List<string> vastused = new List<string>();
-                string sisend;
-                do
-                {
-                    Console.Write($"\n{fraas}\nSisesta märksõna: ");
-                    sisend = Console.ReadLine();
-                    vastused.Add(sisend);
+            //static void KuniMärksõnani(string märksõna, string fraas)
+            //{
+            //    List<string> vastused = new List<string>();
+            //    string sisend;
+            //    do
+            //    {
+            //        Console.Write($"\n{fraas}\nSisesta märksõna: ");
+            //        sisend = Console.ReadLine();
+            //        vastused.Add(sisend);
 
-                    if (sisend == märksõna)
-                    {
-                        Console.WriteLine("Õige vastus, tubli!");
-                    }
+            //        if (sisend == märksõna)
+            //        {
+            //            Console.WriteLine("Õige vastus, tubli!");
+            //        }
 
-                } while (sisend != märksõna);
+            //    } while (sisend != märksõna);
 
-                Console.WriteLine("\nKõik sisetatud vastused:");
-                foreach (var vastus in vastused)
-                {
-                    Console.WriteLine(vastus);
-                }
-            }
+            //    Console.WriteLine("\nKõik sisetatud vastused:");
+            //    foreach (var vastus in vastused)
+            //    {
+            //        Console.WriteLine(vastus);
+            //    }
+            //}
 
-            KuniMärksõnani("elevant", "Osta elevant ära!");
+            //KuniMärksõnani("elevant", "Osta elevant ära!");
 
 
 
             /************************************************************/
             // Ülesanne 5 - Arvamise mäng
+
+            //static void ArvaArv()
+            //{
+            //    Random rnd = new Random();
+            //    int arv = rnd.Next(1, 101);
+            //    int katsed = 5;
+            //    int vastus;
+
+            //    do
+            //    {
+            //        Console.Write("Arva ära arv (1-100): ");
+            //        vastus = int.Parse(Console.ReadLine());
+
+            //        if (vastus > arv) Console.WriteLine("Liiga suur!");
+            //        else if (vastus < arv) Console.WriteLine("Liiga väike!");
+            //        else
+            //        {
+            //            Console.WriteLine("\nÕige arv!");
+            //            return;
+            //        }
+            //        katsed--;
+            //    } while (katsed > 0);
+
+            //    Console.WriteLine($"\nKaotasid! Õige arv oli {arv}");
+            //}
+
+            //ArvaArv();
+
+
+
+            /************************************************************/
+            // Ülesanne 9 - Arvude ruudud
+
+
 
 
 
