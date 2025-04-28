eb313d6552c61c54993744495556b72324913a72
mariasmolina <mariia.smolina@gmail.com>
2025-04-28 18:36:54 +0300

    Ülesanne 1 (Juhuslike arvude ruudud) - tehtud

--- 3. osa - Kordused, massiivid ja klassid/MainClass_ylesanne.cs
+++ 3. osa - Kordused, massiivid ja klassid/MainClass_ylesanne.cs
@@ -10,6 +10,22 @@ namespace TARgv24_C_Sharp._3._osa___Kordused__massiivid_ja_klassid
     {
         public static void Main(string[] args)
         {
+            /************************************************************/
+            // Ülesanne 1 - Juhuslike arvude ruudud (Случайные квадраты чисел)
+
+            (int[] ruudud, int[] arvud) = ArvuTöötlus.GenereeriRuudud(-100, 100); // получаем два массива
+
+            for (int i = 0; i < ruudud.Length; i++)
+            {
+                Console.WriteLine($"{arvud[i]} -> {ruudud[i]}");
+            }
+
+            Console.ReadKey();
+
+
+
+            /************************************************************/
+            // Ülesanne 2 - "Osta elevant ära!"
 
         }
     }
