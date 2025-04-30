a4cbf150d52bfcd1e72a4f54f928fad1d70ed443
opilane <opilane@HP-CZC2349HTN>
2025-04-30 15:27:45 +0300

    tunnitöö tehtud

---
+++ Kangelane/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C_Sharp.Kangelane
+{
+    static void Main(string[] args)
+    {
+        List<Kangelane> kangelased = new List<Kangelane>();
+
+
+    }
+}
